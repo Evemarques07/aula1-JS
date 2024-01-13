c7d16947b35ce4133b3955dabe1c2f5605ff38b1
Evemarques07 <150521195+Evemarques07@users.noreply.github.com>
2024-01-13 14:00:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+// Q 1
+
+// let nome = prompt("Digite seu nome")
+
+// console.log(`Bem-vindo ${nome}`)
+
+// Q 2
+
+// let num1 = Number(prompt("Digite o valor do primeiro número"))
+// let num2 = Number(prompt("Digite o valor do segundo número"))
+
+// let resultado = num1 + num2
+
+// console.log("A soma desses dois números é: "+ resultado)
+
+// Q 3
+
+// let idade = Number(prompt("Digite sua Idade"))
+
+// let ano_atual = new Date().getFullYear()
+
+// let ano_nascimento = ano_atual - +idade
+
+// console.log(`O ano que você nasceu foi ${ano_nascimento}`)
+
+// Q 4
+
+// const pi = 3.14159265358979323846
+// let raio = prompt("Digite o valor do raio")
+
+// console.log("Área do circulo iguual a: " + pi* (raio**2))
+
+// Q 5
+
+
+// let dolar = Number(prompt("Digite o valor do do Dólar"))
+// let euro = 0.85
+
+// let cotacao = euro * dolar
+// console.log(`Cotação hoje essa Qtd de dólar vale: ${cotacao} Euros`) 
+
+// Q 6
+
+// let valor_produto = Number(prompt("Digite o valor do produto"))
+// let desconto = 0.1
+// let resulta_desconto = valor_produto - (valor_produto*desconto)
+// console.log(`Valor com desconto é R$ ${resulta_desconto}`)
+
+// Q 7 
+
+// let metros = Number(prompt("Digite o valor em metros"))
+// let cent = metros * 100
+
+// console.log(`Valor em centímetros é ${cent}cm`)
+
+// Desafio
+
+// let valor_hora = Number(prompt("Digite o valor da hora trabalhada"))
+// let horas_mes = Number(prompt("Digite a quantidade de horas trabalhadas no mês"))
+
+// let ir = 0.11
+// let inss = 0.08
+// let sindicato = 0.05
+
+
+// let salario_bruto = valor_hora*horas_mes
+
+// let desc_ir = salario_bruto*ir
+// let desc_inss = salario_bruto*inss
+// let desc_sindi = salario_bruto*sindicato
+
+// let total_desconto = desc_inss+desc_ir+desc_sindi
+
+// let sal_liq = salario_bruto-total_desconto
+
+// console.log(`Salário Bruto é R$ ${salario_bruto}`)
+// console.log(`Você pagou R$ ${salario_bruto*inss} de INSS` )
+// console.log(`Você pagou R$ ${salario_bruto*ir} de Imposto de renda` )
+// console.log(`Você pagou R$ ${salario_bruto*sindicato} ao Sindicato`)
+// console.log(`Total de desconto R$ ${total_desconto}`)
+// console.log(`Seu salário líquido é R$ ${sal_liq}`)
+
+// Extra
+
+// let peso = Number(prompt("Digite seu peso"))
+// let altura = Number(prompt("Digite sua altura"))
+// let imc = peso / (altura**2)
+// console.log(`Seu IMC é ${imc}`)
